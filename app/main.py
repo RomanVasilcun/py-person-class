afe1d0fc116ddce582db91429c8b226397ccc83f
Roman Vasilcun <roman.vasilcun@gmail.com>
2025-02-27 16:07:34 +0200

    "Solution"

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 class Person:
     people = {}
-    def  __init__(self, name: str, age: int):
-        self.__name = name
-        self.__age = age
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
         Person.people[name] = self
 
 
@@ -12,17 +13,16 @@ def create_person_list(people: list) -> list:
         person = Person(person_dict["name"], person_dict["age"])
         person_instances.append(person)
     for person_dict, person_instance in zip(people, person_instances):
-        if 'wife' in person_dict and person_dict['wife'] is not None:
-            wife_name = person_dict['wife']
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            wife_name = person_dict["wife"]
             if wife_name in Person.people:
                 person_instance.wife = Person.people[wife_name]
             else:
                 person_instance.wife = None
-        if 'husband' in person_dict and person_dict['husband'] is not None:
-            husband_name = person_dict['husband']
+        if "husband" in person_dict and person_dict["husband"] is not None:
+            husband_name = person_dict["husband"]
             if husband_name in Person.people:
                 person_instance.husband = Person.people[husband_name]
             else:
                 person_instance.husband = None
     return person_instances
-
