d718f158808b2edb0c950bb101517d159b7ab06b
Roman Vasilcun <roman.vasilcun@gmail.com>
2025-02-27 15:53:31 +0200

    "Solution"

--- app/main.py
+++ app/main.py
@@ -12,9 +12,17 @@ def create_person_list(people: list) -> list:
         person = Person(person_dict["name"], person_dict["age"])
         person_instances.append(person)
     for person_dict, person_instance in zip(people, person_instances):
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            person_instance.wife = Person.people[person_dict["wife"]]
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            person_instance.husband = Person.people[person_dict["husband"]]
+        if 'wife' in person_dict and person_dict['wife'] is not None:
+            wife_name = person_dict['wife']
+            if wife_name in Person.people:
+                person_instance.wife = Person.people[wife_name]
+            else:
+                person_instance.wife = None
+        if 'husband' in person_dict and person_dict['husband'] is not None:
+            husband_name = person_dict['husband']
+            if husband_name in Person.people:
+                person_instance.husband = Person.people[husband_name]
+            else:
+                person_instance.husband = None
     return person_instances
 
